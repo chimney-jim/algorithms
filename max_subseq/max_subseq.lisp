5b24ccf9331fdcd58d147a69b4a5302b2bcc331c
Jim Fitzgerald <dumples5@gmail.com>
2012-06-13 13:35:38 -0400

    working on stuff still

--- max_subseq/max_subseq.lisp
+++ max_subseq/max_subseq.lisp
@@ -1,7 +1,16 @@
+(defpackage :max-subseq
+  (:nicknames :max-subseq)
+  (:use :cl))
+
+(in-package :max-subseq)
+
 (defparameter things '(4 -3 5 -2 -1 2 6 -2))
 (defparameter subthings1 (list))
 (defparameter subthings2 (list))
 (defparameter master-list (list))
+(defparameter x 1)
+(defvar left-max-sum)
+(defvar right-max-sum)
 
 (defun list-split (to-split)
   (setf subthings1
@@ -16,3 +25,23 @@
     (setf master-list (append master-list (loop for x on to-sum collect x)))
     (setf master-list (append master-list (loop for x on (reverse to-sum) collect x)))
     (unless (eq shorter-list NIL) (set-master-list shorter-list))))
+
+(defun comb (m list fn)
+  (labels ((comb1 (l c m)
+                  (when (>= (length l) m)
+                    (if (zerop m) (return-from comb1 (funcall fn c)))
+                    (comb1 (cdr l) c m)
+                    (comb1 (cdr l) (cons (first l) c) (1- m)))
+                  (print l)
+                  (print c)
+                  (print m)
+                  (format t "~%end pass ~a" x)
+                  (setf x (+ 1 x))))
+    (comb1 list nil m)))
+
+(defun max-sum (list left right)
+  (let (max-left 0) (max-right 0) (left-sum 0) (right-sum 0) (center (/ (+ left right) 2))
+    (if (= left right)
+      (first list)
+      (nil))
+    (loop for i to left
