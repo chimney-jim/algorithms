8d237f08a93feab254380cce8579d18c864c39f7
Jim Fitzgerald <dumples5@gmail.com>
2012-06-21 16:01:59 -0400

    err: value of m is not of type NUMBER

--- LCS/lcs.lisp
+++ LCS/lcs.lisp
@@ -11,9 +11,10 @@
         (format t "~%")))
 
 (defun lcs (lst1 lst2)
-  (let ((m (- (length lst1) 1)) (n (- (length lst2) 1)))
-    (setf b (make-array '(m n)))
-    (setf c (make-array '(m n)))
+  (let ((m (- (length lst1) 1)) 
+        (n (- (length lst2) 1))
+        (b (make-array '(m n)))
+        (c (make-array '(m n))))
     (loop for i from 0 to m
           do (setf (aref c i 0) 0))
     (loop for j from 0 to n
@@ -21,12 +22,15 @@
     (loop for i from 0 to m
           do (loop for j from 0 to n
                    do (if (eq (car lst1) (car lst2))
-                        ((setf (aref c i j) (+ (aref c (- i 1) (- j 1)) 1))
-                         (setf (aref b i j) 1))
-                        ((if (>= (aref c (- i 1) j) (aref c i (- j 1)))
-                           ((setf (aref c i j) (aref c (- i 1) j))
-                            (setf (aref b i j) 2))
-                           ((setf (aref c i j) (aref c i (- j 1)))
-                            (setf (aref b i j) 0)))))))
+                        (lambda (c b i j) 
+                          (setf (aref c i j) (+ (aref c (- i 1) (- j 1)) 1))
+                           (setf (aref b i j) 1) c b i j)
+                        (if (>= (aref c (- i 1) j) (aref c i (- j 1)))
+                          (lambda (c b i j) 
+                            (setf (aref c i j) (aref c (- i 1) j))
+                             (setf (aref b i j) 2) c b i j)
+                           (lambda (c b i j) 
+                             (setf (aref c i j) (aref c i (- j 1)))
+                              (setf (aref b i j) 0) c b i j)))))
     (show-board c)
     (show-board b)))
