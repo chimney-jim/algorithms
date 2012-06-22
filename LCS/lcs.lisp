266301f5a71d6e784a7c2c0d94604700624f33c5
Jim Fitzgerald <dumples5@gmail.com>
2012-06-22 15:53:28 -0400

    used let to bind b and c, but no dice

--- LCS/lcs.lisp
+++ LCS/lcs.lisp
@@ -10,19 +10,19 @@
 (defun lcs (lst1 lst2)
   (let ((m (length lst1)) 
         (n (length lst2)))
-    (setf b (make-array (list m n) :initial-element 0))
-    (setf c (make-array (list m n) :initial-element 0))
-    (loop for i from 1 to (- m 1)
-          do (loop for j from 1 to (- n 1)
-                   do (if (eq (nth i lst1) (nth j lst2))
-                        (progn
-                          (setf (aref c i j) (+ (aref c (- i 1) (- j 1)) 1))
-                          (setf (aref b i j) 1))
-                        (if (>= (aref c (- i 1) j) (aref c i (- j 1)))
-                          (progn 
-                            (setf (aref c i j) (aref c (- i 1) j))
-                            (setf (aref b i j) 2))
-                           (progn 
-                             (setf (aref c i j) (aref c i (- j 1)))
-                             (setf (aref b i j) 0))))))
+    (let ((b (make-array (list m n) :initial-element 0))
+          (c (make-array (list m n) :initial-element 0)))
+      (loop for i from 1 to (- m 1)
+            do (loop for j from 1 to (- n 1)
+                     do (if (eq (nth i lst1) (nth j lst2))
+                          (progn
+                            (setf (aref c i j) (+ (aref c (- i 1) (- j 1)) 1))
+                            (setf (aref b i j) 1))
+                          (if (>= (aref c (- i 1) j) (aref c i (- j 1)))
+                            (progn 
+                              (setf (aref c i j) (aref c (- i 1) j))
+                              (setf (aref b i j) 2))
+                            (progn 
+                              (setf (aref c i j) (aref c i (- j 1)))
+                              (setf (aref b i j) 0)))))))
     (list b c)))
