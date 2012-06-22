14a18450ea120e8fc2fbb0e0589a522b0352d788
Jim Fitzgerald <dumples5@gmail.com>
2012-06-22 13:53:01 -0400

    arrays are filled with all 0s

--- LCS/lcs.lisp
+++ LCS/lcs.lisp
@@ -1,5 +1,6 @@
 (defpackage :lcs
-  (:use :cl))
+  (:use :cl)
+  (:export :lcs))
 
 (defvar list1 '(A B C B D A B))
 (defvar list2 '(B D C A B A))
@@ -14,26 +15,26 @@
         (format t "~%")))
 
 (defun lcs (lst1 lst2)
-  (let ((m (- (length lst1) 1)) 
-        (n (- (length lst2) 1)))
+  (let ((m (length lst1)) 
+        (n (length lst2)))
     (setf b (make-array (list m n)))
     (setf c (make-array (list m n)))
-    (loop for i from 0 to m
+    (loop for i from 0 to (- m 1)
           do (setf (aref c i 0) 0))
-    (loop for j from 0 to n
+    (loop for j from 0 to (- n 1)
           do (setf (aref c 0 j) 0))
-    (loop for i from 0 to m
-          do (loop for j from 0 to n
+    (loop for i from 1 to (- m 1)
+          do (loop for j from 1 to (- n 1)
                    do (if (eq (car lst1) (car lst2))
                         (lambda (c b i j) 
                           (setf (aref c i j) (+ (aref c (- i 1) (- j 1)) 1))
-                           (setf (aref b i j) 1) c b i j)
+                          (setf (aref b i j) 1) c b i j)
                         (if (>= (aref c (- i 1) j) (aref c i (- j 1)))
                           (lambda (c b i j) 
                             (setf (aref c i j) (aref c (- i 1) j))
-                             (setf (aref b i j) 2) c b i j)
+                            (setf (aref b i j) 2) c b i j)
                            (lambda (c b i j) 
                              (setf (aref c i j) (aref c i (- j 1)))
-                              (setf (aref b i j) 0) c b i j)))))
-    (show-board c)
-    (show-board b)))
+                             (setf (aref b i j) 0) c b i j)))))
+    (print c)
+    (print b)))
