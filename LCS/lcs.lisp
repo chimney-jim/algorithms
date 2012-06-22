126b5a81db2442a90532dd2ee3503d846b4a26b8
Jim Fitzgerald <dumples5@gmail.com>
2012-06-22 19:16:15 -0400

    lcs funtion working properly, now to add a print function

--- LCS/lcs.lisp
+++ LCS/lcs.lisp
@@ -2,19 +2,19 @@
   (:use :cl)
   (:export :lcs))
 
-(defvar list1 '(1 3 5 4 2 6 7))
-(defvar list2 '(8 3 4 9 2 1))
+(defvar list1 '(1 2 3 2 4 1 2))
+(defvar list2 '(2 4 3 1 2 1))
 
 (in-package :lcs)
 
 (defun lcs (lst1 lst2)
-  (let ((m (length lst1)) 
-        (n (length lst2)))
-    (let ((b (make-array (list m n) :initial-element 0))
-          (c (make-array (list m n) :initial-element 0)))
-      (loop for i from 1 to (- m 1)
-            do (loop for j from 1 to (- n 1)
-                     do (if (eq (nth i lst1) (nth j lst2))
+  (let* ((m (length lst1)) 
+         (n (length lst2))
+         (b (make-array (list (+ m 1) (+ n 1)) :initial-element 0))
+         (c (make-array (list (+ m 1) (+ n 1)) :initial-element 0)))
+    (loop for i from 1 to m
+            do (loop for j from 1 to n
+                     do (if (eq (nth (- i 1) lst1) (nth (- j 1) lst2))
                           (progn
                             (setf (aref c i j) (+ (aref c (- i 1) (- j 1)) 1))
                             (setf (aref b i j) 1))
@@ -24,5 +24,10 @@
                               (setf (aref b i j) 2))
                             (progn 
                               (setf (aref c i j) (aref c i (- j 1)))
-                              (setf (aref b i j) 0)))))))
-    (list b c)))
+                              (setf (aref b i j) 0))))))
+   (list c b)))
+
+;;(defvar dir-list (lcs list1 list2))
+
+;;(defvar print-lcs (dir-list lst1 lst2)
+;;  (let ((m (length 
